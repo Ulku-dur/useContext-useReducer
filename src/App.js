1d325cf2e379d5ab97f07ba3b6e43e4f5ac16c01
Ülkü <93094752+Ulku-dur@users.noreply.github.com>
2024-09-11 18:51:47 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import { Router, Link } from "@reach/router";
 
-function App() {
+import { ShopProvider } from "./ShopContext";
+import Products from "./Products";
+import Cart from "./Cart";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ShopProvider>
+      <Wrapper>
+        <TitleWrapper>
+          <h1>useReducer Hook</h1>
+          
+        </TitleWrapper>
+        <LinksWrapper>
+          <Link to="/">Home</Link>
+          <Link to="/cart">Cart</Link>
+        </LinksWrapper>
+        <Router>
+          <Products path="/" />
+          <Cart path="/cart" />
+        </Router>
+      </Wrapper>
+    </ShopProvider>
   );
-}
+};
 
 export default App;
+
+const Wrapper = styled.div`
+  font-family: "Roboto";
+  margin: 40px;
+
+  display: grid;
+  row-gap: 20px;
+  justify-content: center;
+`;
+
+const TitleWrapper = styled.div`
+  * {
+    margin: 0;
+  }
+
+  display: grid;
+  row-gap: 10px;
+
+  a {
+    text-decoration: none;
+    font-weight: bold;
+    color: black;
+  }
+`;
+
+const LinksWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-evenly;
+
+  a {
+    text-decoration: none;
+    color: #bb7250;
+
+    :hover {
+      color: #bb7250;
+      font-weight: bold;
+      text-decoration: underline;
+    }
+  }
+`;
